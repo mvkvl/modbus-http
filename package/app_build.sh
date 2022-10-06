f8a96f65cb6ee5dbea99e693c3190aa4b1b90832
Mikhail <kami@slink.ws>
2022-10-06 15:47:01 +1000

    automatically build for supported platforms & package linux version to debian packages (#6)

---
+++ package/app_build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# go tool dist list
+
+# GOOS
+#Linux 	        linux
+#MacOS X 	      darwin
+#Windows 	      windows
+#FreeBSD 	      freebsd
+#NetBSD 	      netbsd
+#OpenBSD 	      openbsd
+#DragonFly BSD 	dragonfly
+#Plan 9 	      plan9
+#Native Client 	nacl
+#Android 	      android
+
+# GOARCH
+#x386 	                  386
+#AMD64 	                  amd64
+#AMD64 с 32-указателями 	amd64p32
+#ARM 	                    arm
+#ARM 	                    arm64
+
+# GOARM
+# armel (softfloat)               GOARM=5
+# armhf (hardware floating point) GOARM=6 / GOARM=7
+
+DSTPATH=$1
+if [ -z $DSTPATH ]; then
+  DSTPATH="./distr"
+fi
+
+function cleanup {
+  rm -rf ${DSTPATH}/bin || true
+}
+
+function build() {
+  mkdir -p ${DSTPATH}/bin/$4/$5/
+  GOOS=$1 GOARCH=$2 GOARM=$3 go build -o ${DSTPATH}/bin/$4/$5/mbridge ..
+}
+
+cleanup                                     && \
+build linux   amd64   ""    linux   amd64   && \
+build darwin  arm64   ""    macos   amd64   && \
+build darwin  amd64   ""    macos   m1      && \
+build linux   arm     7     linux   armhf   && \
+build linux   arm64   7     linux   armhf64 && \
+echo "Build complete"
+
---
+++ package/podman_build.sh
@@ -0,0 +1,1 @@
+podman build . -t mvkvl/debian-golang-systemd
