40d2dffd7d1d9fbb29a9892a3f78fcec262de011
Mikhail <kami@slink.ws>
2022-10-07 08:14:12 +1000

    added device alias support (#7)

--- model/model.go
+++ model/model.go
@@ -256,6 +256,7 @@ func (c Channel) findDeviceByTitle(title string) (*Device, error) {
 type Device struct {
 	SlaveId   uint8      `json:"slave_id,omitempty"`
 	Title     string     `json:"title,omitempty"`
+	Alias     string     `json:"alias,omitempty"`
 	Registers []Register `json:"registers,omitempty"`
 }
 
@@ -266,6 +267,39 @@ func (d *Device) findRegisterByTitle(title string) (*Register, error) {
 		}
 	}
 	return nil, errors.New(fmt.Sprintf("no register found for title '%s'", title))
+}
+
+func (d *Device) UnmarshalJSON(data []byte) (err error) {
+	var obj map[string]interface{}
+	if err := json.Unmarshal(data, &obj); err != nil {
+		return err
+	}
+	var device Device
+
+	if nil != obj["title"] {
+		device.Title = fmt.Sprint(obj["title"])
+	}
+	if nil != obj["alias"] {
+		device.Alias = fmt.Sprint(obj["alias"])
+	} else {
+		device.Alias = device.Title
+	}
+	if nil != obj["slave_id"] {
+		v, _ := strconv.Atoi(fmt.Sprint(obj["slave_id"]))
+		device.SlaveId = uint8(v)
+	}
+	if nil != obj["registers"] {
+		r := obj["registers"]
+		rj, err := json.Marshal(r)
+		if nil != err {
+			return err
+		}
+		if err := json.Unmarshal(rj, &device.Registers); err != nil {
+			return err
+		}
+	}
+	*d = device
+	return nil
 }
 
 // endregion
